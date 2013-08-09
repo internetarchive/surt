969977f798823d75377b742e43a192fdcff49d0d
rajbot <raj@archive.org>
2013-08-09 21:31:08 +0000

    Update version number to 0.2

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='surt',
-      version='0.1',
+      version='0.2',
       author='rajbot',
       author_email='raj@archive.org',
       description='Sort-friendly URI Reordering Transform (SURT) python package.',
