26b0c49e5aed7bfbdca3fc340df8bfc2e671d067
Ilya Kreymer <ilya@archive.org>
2013-09-18 23:50:26 +0000

    add exception handling to canonicalize.py

--- surt/canonicalize.py
+++ surt/canonicalize.py
@@ -3,12 +3,22 @@
 
 # A script for canonicalize (non-surt currently) a stream of urls from stdin
 
-from surt import surt
-
 import sys
+import surt
+
+
+encountered_error = False
 
 for line in sys.stdin:
-  url = line.rstrip()
+    url = line.rstrip()
 
-  print surt(url, surtMode=False, includeScheme=False)
+    try:
+        print surt.surt(url, surtMode=False, includeScheme=False)
+    except:
+        sys.stderr.write("Error: Invalid url %s\n" % url)
+	encountered_error = True
 
+if encountered_error:
+    exit(1)
+
+exit(0)
