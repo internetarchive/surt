746f506dd6f0798adaa5bfd92101b73ed00f2831
rajbot <raj@archive.org>
2014-04-01 23:16:11 +0000

    Add LICENSE. Closes rajbot/surt#1

--- surt/handyurl.py
+++ surt/handyurl.py
@@ -1,6 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# Copyright(c)2012-2013 Internet Archive. Software license AGPL version 3.
+#
+# This file is part of the `surt` python package.
+#
+#     surt is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU Affero General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     surt is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU Affero General Public License for more details.
+#
+#     You should have received a copy of the GNU Affero General Public License
+#     along with surt.  If not, see <http://www.gnu.org/licenses/>.
+#
+#     The surt source is hosted at https://github.com/internetarchive/surt
+
 import re
 import tldextract
 from urlparse import urlsplit
